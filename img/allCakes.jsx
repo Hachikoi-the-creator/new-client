eac2619be3e7b210a528080b4d7450352ed119fc
Hachikoi-the-creator <facka72@gmail.com>
2022-05-20 19:37:18 -0500

    first commit

---
+++ img/allCakes.jsx
@@ -0,0 +1,28 @@
+import maru1 from "./maru1.png"; //XV yrs//
+import maru2 from "./maru2.png"; //XV yrs//
+import maru3 from "./maru3.png"; //XV yrs//
+import maru4 from "./maru4.png"; //wedding
+import maru5 from "./maru5.png"; //XV yrs//
+import maru6 from "./maru6.png"; //wedding
+import maru7 from "./maru7.png"; //wedding
+
+import oval1 from "./oval1.png"; //wedding
+import oval2 from "./oval2.png"; //XV yrs//
+import oval3 from "./oval3.png"; //wedding
+import oval4 from "./oval4.png"; //wedding
+import oval5 from "./oval5.png"; //wedding
+
+export {
+  maru1,
+  maru2,
+  maru3,
+  maru4,
+  maru5,
+  maru6,
+  maru7,
+  oval1,
+  oval2,
+  oval3,
+  oval4,
+  oval5,
+};
